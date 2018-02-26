9946d97f33c6031027b1d94c7a9ca3bf2e441651
David Carslaw <david.carslaw@york.ac.uk>
2018-02-26 21:14:41 +0000

    version 2.2-3

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
- .onLoad <- function (...) {
+.onLoad <- function(...) {
 
- ##  packageStartupMessage("\ttype citation(\"openair\") for how to cite openair")
-
- }
+  ##  packageStartupMessage("\ttype citation(\"openair\") for how to cite openair")
+  utils::globalVariables(c("variable", "value", "res", "site_pol",
+                           ".", "data", "fit", "grp", "intercept", "out",
+                           "seslope", "slope", "weekday"))
+}
